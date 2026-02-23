5e52d1c45ae32fdf72edc824a710e306d4fd18dd
reallyr0cks <brincenathan@gmail.com>
2026-02-23 15:04:40 -0500

    Update page.tsx

--- app/journalism/[slug]/page.tsx
+++ app/journalism/[slug]/page.tsx
@@ -1,4 +1,5 @@
 // import { Button } from "@/components/ui/Button";
+import SubmissionCarousel from "@/components/SubmissionCarousel";
 import { RichTextRenderer } from "@/components/ui/RichText";
 import { Submission } from "@/lib/types";
 import { client } from "@/sanity/lib/client";
@@ -10,24 +11,28 @@ export default async function Page({
 }) {
     const { slug } = await params;
     const query = `
-    *[_type == "submission" && status == "Published" && slug.current == $slug]{
-        author->{
-            name,
-            anonymous
-        },
+*[_type == "submission" && status == "Published" && slug.current == $slug]{
+    author->{
+        name,
+        anonymous
+    },
+    title,
+    content,
+    category->{
         title,
-        content,
-        category->{
-            title,
-            "slug": slug.current
-        },
-        tags->{
-            name
-        },
-        featured,
-        submittedDate
-
-    }[0]`;
+        "slug": slug.current
+    },
+    tags->{
+        name
+    },
+    featured,
+    submittedDate,
+    images[]{
+      asset->{
+        url
+      }
+    }
+}[0]`;
     const submission = await client.fetch<Submission>(query, {
         slug: slug,
     });
@@ -35,13 +40,15 @@ export default async function Page({
     console.log(submission);
     return (
         <div>
-            <article className="prose mx-auto">
-                <h1>{submission.title}</h1>
-                {/* Content renderer */}
-                <div className="mx-5 text-foreground">
-                    <RichTextRenderer value={submission.content} />
-                </div>
-            </article>
+<article className="prose mx-auto">
+    <h1>{submission.title}</h1>
+
+    <div className="mx-5 text-foreground">
+        <RichTextRenderer value={submission.content} />
+    </div>
+
+    <SubmissionCarousel images={submission.images} />
+</article>
             {/* <div className="md:col-span-2">
                 <div className="flex items-center gap-4 mb-4">
                     <span
@@ -107,6 +114,7 @@ export default async function Page({
                     </div>
                 </div>
             </div> */}
+            
         </div>
     );
 }
