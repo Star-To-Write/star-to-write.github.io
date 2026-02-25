d15c2551f4a6c1c4ac3e56652ccd2499e3cef0c0
reallyr0cks <brincenathan@gmail.com>
2026-02-25 17:45:19 -0500

    Update page.tsx

--- app/journalism/[slug]/page.tsx
+++ app/journalism/[slug]/page.tsx
@@ -1,70 +1,55 @@
-"use client"
+// import { Button } from "@/components/ui/Button";
+import SubmissionCarousel from "./components/SubmissionCarousel";
+import { RichTextRenderer } from "@/components/ui/RichText";
+import { Submission } from "@/lib/types";
+import { client } from "@/sanity/lib/client";
 
-import { useState } from "react"
-import Image from "next/image"
-
-type ImageType = {
-  asset: {
-    url: string
-  }
-}
-
-export default function SubmissionCarousel({
-  images,
+export default async function Page({
+    params,
 }: {
-  images: ImageType[]
+    params: Promise<{ slug: string }>;
 }) {
-  const [current, setCurrent] = useState(0)
+    const { slug } = await params;
+    const query = `
+*[_type == "submission" && status == "Published" && slug.current == $slug]{
+    author->{
+        name,
+        anonymous
+    },
+    title,
+    content,
+    category->{
+        title,
+        "slug": slug.current
+    },
+    tags->{
+        name
+    },
+    featured,
+    submittedDate,
+    images[]{
+      asset->{
+        url
+      }
+    }
+}[0]`;
+    const submission = await client.fetch<Submission>(query, {
+        slug: slug,
+    });
 
-  if (!images || images.length === 0) return null
+    console.log(submission);
+    return (
+        <div>
+<article className="prose mx-auto">
+    <h1>{submission.title}</h1>
 
-  const prev = () => {
-    setCurrent((current - 1 + images.length) % images.length)
-  }
-
-  const next = () => {
-    setCurrent((current + 1) % images.length)
-  }
-
-  return (
-    <div className="relative w-full max-w-4xl mx-auto">
-
-      {/* IMAGE */}
-      <div className="relative w-full h-[500px]">
-        <Image
-          src={images[current].asset.url}
-          alt="Submission image"
-          fill
-          className="object-contain"
-        />
-      </div>
-
-      {/* LEFT FIGMA BUTTON */}
-      <button
-        onClick={prev}
-        className="absolute left-6 top-1/2 -translate-y-1/2"
-      >
-        <Image
-          src="/images/left-arrow.png"
-          alt="Previous"
-          width={60}
-          height={60}
-        />
-      </button>
-
-      {/* RIGHT FIGMA BUTTON */}
-      <button
-        onClick={next}
-        className="absolute right-6 top-1/2 -translate-y-1/2"
-      >
-        <Image
-          src="/images/right-arrow.png"
-          alt="Next"
-          width={60}
-          height={60}
-        />
-      </button>
-
+    <div className="mx-5 text-foreground">
+        <RichTextRenderer value={submission.content} />
     </div>
-  )
+
+    <SubmissionCarousel images={submission.images} />
+</article>
+            {}
+        </div>
+    );
 }
