17871072c799dfb380f8eaa83f227d400b36f5aa
rinrinluvs <173377333+rinrinluvs@users.noreply.github.com>
2026-04-12 06:18:32 +0700

    Fix grid issue on members page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -339,7 +339,7 @@ export default function AboutUs() {
                                 {department.name}
                             </h3>
 
-                            <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 justify-center">
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-center">
                                 {department.members.map((member, index) => (
                                     <div
                                         key={index}
