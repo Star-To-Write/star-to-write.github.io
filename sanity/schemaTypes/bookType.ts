589f06970d5f7a6ec58568d8147a656a6e992e75
rinrinluvs <rinrinluvs@gmail.com>
2026-07-14 04:08:28 +0700

    Establish books schema + a little cheeky basic begin to the books

---
+++ sanity/schemaTypes/bookType.ts
@@ -0,0 +1,94 @@
+import { BookIcon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+
+export const bookType = defineType({
+  name: "book",
+  title: "Book Management",
+  type: "document",
+  icon: BookIcon,
+  fields: [
+    defineField({
+      name: "title",
+      title: "Book Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug" as const,
+      options: { source: "name" },
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "cover",
+      title: "Book Cover",
+      type: "image" as const,
+      options: {
+        hotspot: true, // enables smart cropping
+      },
+    }),
+    defineField({
+      name: "type",
+      title: "Book Type",
+      type: "array" as const,
+      of: [
+        {
+          type: "string",
+          options: {
+            list: [
+              {
+                type: "string",
+                options: {
+                  list: [
+                    {
+                      title: "Children's Book",
+                      value: "Children's Book",
+                    },
+                    {
+                      title: "Short Novel",
+                      value: "Short Novel",
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      ],
+      validation: (Rule) => Rule.required(),
+    }),
+    // author
+    defineField({
+      name: "author",
+      type: "reference" as const,
+      to: [{ type: "author" as const }],
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "synopsis",
+      title: "Synopsis",
+      type: "text" as const,
+      rows: 3,
+    }),
+    defineField({
+      title: "Price",
+      name: "price",
+      type: "number" as const,
+	  validation: (Rule) => Rule.required()
+      .min(0)
+      .precision(2)
+    }),
+	defineField({
+		title: "Link",
+		name: "link",
+		type: "string",
+		validation: (Rule) => Rule.required()
+	})
+
+    //     type: "boolean" as const,
+    //     initialValue: false,
+    // }),
+  ],
+});
